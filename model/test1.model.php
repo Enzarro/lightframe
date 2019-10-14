87814792710a8676243f06dd6df75599a24ab85d
enzarro <spreadingmadness@gmail.com>
2019-10-14 18:47:33 -0300

    ahi van de nuevo

--- model/test1.model.php
+++ model/test1.model.php
@@ -1,5 +1,11 @@
 <?php
 
 class test1_model {
-    
+    function get($id) {
+        global $config;
+        $_DB = new database($config->database);
+
+        //Grilla según ID
+        return $_DB->queryToArray("SELECT * FROM test1 WHERE id = {$id}");
+    }
 }
--- model/test2.model.php
+++ model/test2.model.php
@@ -1,5 +1,11 @@
 <?php
 
 class test2_model {
-    
+    function get($id) {
+        global $config;
+        $_DB = new database($config->database);
+
+        //Grilla según ID
+        return $_DB->queryToArray("SELECT * FROM test2 WHERE id = {$id}");
+    }
 }
